f21b6636bd2c0513fa81e58e9b402880b2478d3e
Nandor Dudas <nandor.dudas@gmail.com>
2023-04-08 09:38:22 +0000

    chore: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
+const root = fileURLToPath(new URL('./src', import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~': root,
+    },
+  },
 })
