5469305966c5488e989e89096bfb10cfca5c3e8e
Nandor Dudas <nandor.dudas@gmail.com>
2023-04-09 07:10:53 +0000

    test: remove unused utility function

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/export */
 import { type RenderOptions, render } from '@testing-library/react'
 import type { PreloadedState } from '@reduxjs/toolkit'
 import type { PropsWithChildren, ReactElement } from 'react'
@@ -10,13 +9,6 @@ import { setupStore } from '~/test/setup-store'
 interface ExtendedRenderOptions<State extends RootState> extends Omit<RenderOptions, 'queries'> {
   preloadedState?: PreloadedState<State>
   store?: typeof store
-}
-
-function customRender(ui: ReactElement, options: RenderOptions = {}) {
-  return render(ui, {
-    wrapper: ({ children }) => children,
-    ...options,
-  })
 }
 
 export function renderWithProviders(
@@ -44,4 +36,3 @@ export function renderWithProviders(
 
 export * from '@testing-library/react'
 export { default as userEvent } from '@testing-library/user-event'
-export { customRender as render }
